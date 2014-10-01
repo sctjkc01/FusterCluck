58b3fe427b9c4901c50c2531901943949afdd27f
Daniel Law <djl1005@rit.edu>
2014-10-01 00:06:03 -0400

    Player can move
    Also had to replace all 3D physic stuff added with 2D stuff

--- Project/Assets/PlayerControl.cs
+++ Project/Assets/PlayerControl.cs
@@ -7,8 +7,43 @@ public enum Player {
 
 public class PlayerControl : MonoBehaviour {
 
+	private Vector2 forceDirection;
+	public float speed;
+	public float MAX_SPEED; 
+
     // Update is called once per frame
     void Update() {
+
+		Move ();
         
     }
+
+	void Move()
+	{
+
+		forceDirection = Vector2.zero;
+
+		if (Input.GetAxis ("Vertical") > 0) {	
+			forceDirection += (Vector2.up);
+		}
+		if (Input.GetAxis ("Horizontal") < 0) {
+			forceDirection -= (Vector2.right);
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			forceDirection += (Vector2.right);
+		}
+		if (Input.GetAxis ("Vertical") < 0) {
+			forceDirection -= (Vector2.up);
+		}
+		forceDirection *= speed;
+
+		Vector2 force = Vector2.ClampMagnitude(forceDirection, MAX_SPEED);
+
+		rigidbody2D.AddForce(force, ForceMode2D.Impulse);
+
+		rigidbody2D.velocity = Vector2.ClampMagnitude (rigidbody2D.velocity, MAX_SPEED);
+				
+
+
+	}
 }
