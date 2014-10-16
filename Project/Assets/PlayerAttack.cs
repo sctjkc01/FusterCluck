64d6e6f2ae4b72f804c7312e5b450712a54680f1
The Stick <avaera8820@gmail.com>
2014-10-16 12:09:17 -0400

    Player direction fixed
    DEAR LORD OMG FUCK

--- Project/Assets/PlayerAttack.cs
+++ Project/Assets/PlayerAttack.cs
@@ -3,29 +3,47 @@ using System.Collections;
 
 public class PlayerAttack : MonoBehaviour {
 
-	public Component box;
-	public GameObject player;
+    public Component box;
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log (this.gameObject.layer);
-		Debug.Log (box.gameObject.activeSelf);
-		Vector3 pos = new Vector3 (player.transform.position.x, player.transform.position.y + .5f, player.transform.position.z);
-		this.transform.position = pos;
-	}
+    // Use this for initialization
+    void Start() {
 
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject.tag.Equals("Enemy"))
-		{
-			Destroy(collision.gameObject);
-		}
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(this.gameObject.layer);
+        Debug.Log(box.gameObject.activeSelf);
+
+        transform.position = player.transform.position;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(Mathf.Abs(horizontal) > 0.05 || Mathf.Abs(vertical) > 0.05) {
+            Vector3 playerInput = new Vector3(horizontal, vertical);
+            Debug.DrawLine(transform.position, transform.position + playerInput * 5f, Color.red);
+
+            if(playerInput.x > 0) {
+                transform.localScale = new Vector3(1f, -1f, 1f);
+                playerInput.y *= -1;
+            } else {
+                transform.localScale = Vector3.one;
+            }
+
+            Debug.Log("" + Vector3.Angle(Vector3.up, playerInput));
+
+            Quaternion rotation = Quaternion.identity;
+            rotation.eulerAngles = new Vector3(0f, 0f, Vector3.Angle(Vector3.up, playerInput));
+            transform.rotation = rotation;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag.Equals("Enemy")) {
+            Destroy(collision.gameObject);
+        }
+
+    }
 
 }
